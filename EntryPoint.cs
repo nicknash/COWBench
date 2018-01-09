1e8aa0d55d02c263516526e177cc4a7c28ae4821
Nick Nash <nicholas.nash@gmail.com>
2018-01-09 07:18:10 +0000

    Add TODO

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -4,6 +4,8 @@ class EntryPoint
 {
     public static void Main(string[] args)
     {
-        
+        // TODO, simple demonstration of:
+        //  * Time to complete for N readers, M writers (explore N, M)
+        //  * Read/Write latency (repeat, say 10,000 times - full distribution?) for N readers, M writers (explore N, M)
     }
 }
