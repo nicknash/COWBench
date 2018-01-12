65d60bfe6509aede7751ef0f3e23ebfc4b5d97fd
Nick Nash <nicholas.nash@gmail.com>
2018-01-12 07:21:48 +0000

    Add sketch of thoughput and latency benchmark

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -10,19 +10,37 @@ namespace COWBench
         {
             int testThreads = Int32.Parse(args[0]);
             var barrier = new Barrier(testThreads + 1);
+            var list = new MultipleWriterCOWList(); // TODO, pre-alloc this so no growing
+            int numOperations = 10000;
             for (int numReaders = 0; numReaders < testThreads; ++numReaders)
             {
                 for (int numWriters = 0; numWriters < testThreads - numReaders; ++numWriters)
                 {
-                    // TODO, simple demonstration of:
-                    //  * Time to complete for N readers, M writers (explore N, M)
-                    //  * Read/Write latency (repeat, say 10,000 times - full distribution?) for N readers, M writers (explore N, M)
+                    StartThreads(idx => list[idx], numReaders, numOperations, barrier);
+                    StartThreads(value => { list.Add(value); return value; }, numWriters, numOperations, barrier);
+                    
+                    barrier.SignalAndWait();  
+                    var start = Stopwatch.GetTimestamp();
+                    barrier.SignalAndWait(); 
+                    var end = Stopwatch.GetTimestamp();
+                    // Record completion time and num ops
+                    // Record latency distributions
                 }
             }
-            barrier.SignalAndWait(); // start 
         }
 
-        public void Thread(object p)
+        private static void StartThreads(Func<int, int> operation, int numThreads, int numOperations, Barrier barrier)
+        {
+            var threads = new Thread[numThreads];
+            for(int i = 0; i < numThreads; ++i)
+            {
+                var context = new ThreadContext(numOperations, operation, barrier);
+                threads[i] = new Thread(new ParameterizedThreadStart(Thread)){IsBackground = true};
+                threads[i].Start();
+            }
+        }
+
+        public static void Thread(object p)
         {
             var context = (ThreadContext) p;
             context.StartBarrier.SignalAndWait();
--- ThreadContext.cs
+++ ThreadContext.cs
@@ -6,16 +6,18 @@ namespace COWBench
     class ThreadContext
     {
         public readonly int NumOperations;
-        public readonly Action<int> Operation;
+        public readonly Func<int, int> Operation;
+        public readonly int[] OperationResults;
         public readonly Barrier StartBarrier; 
         public readonly long[] Latencies;
 
-        public ThreadContext(int numOperations, Action<int> operation, Barrier barrier)
+        public ThreadContext(int numOperations, Func<int, int> operation, Barrier barrier)
         {
             NumOperations = numOperations;
             Operation = operation;
             Latencies = new long[numOperations];
             StartBarrier = barrier;
+            OperationResults = new int[10];
         }
     }
 }
